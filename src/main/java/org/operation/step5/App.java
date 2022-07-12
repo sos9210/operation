ab184fbb042e0c293e761c1f568ded2f985aa93c
sungsanghyun <sos9215@gmail.com>
2022-07-12 22:20:51 +0900

    step5

---
+++ src/main/java/org/operation/step5/App.java
@@ -0,0 +1,8 @@
+package org.operation.step5;
+
+public class App {
+    public static void main(String[] args) {
+        Moja moja = new MagicMoja();
+        System.out.println(Moja.pullOut());
+    }
+}
---
+++ src/main/java/org/operation/step5/Moja.java
@@ -0,0 +1,8 @@
+package org.operation.step5;
+
+import org.operation.step5.project.Magic;
+
+@Magic
+public interface Moja {
+    String pullOut();
+}
